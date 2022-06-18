b552301a4e17750a932ab881be6df42a63669c91
llattanzi <lauren71442@gmail.com>
2022-06-17 21:02:42 -0400

    create file structure and server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/thoughtful-social-network', {
+    // these are true by default in mongo v6, don't actually need these settings
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// Use this to log mongo queries being executed!
+mongoose.set('debug', true);
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
